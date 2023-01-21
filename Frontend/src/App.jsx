81b7cd959e728b935c94a140f10dd600e79e6581
Don-Alvin <alvindon41@gmail.com>
2023-01-21 23:53:54 +0300

    Login Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './Pages/landingPage/LandingPage'
+import Login from './Pages/loginPage/Login'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} /> 
+      <Route path='/login' element={<Login />} />
     </Routes>
   )
 }
