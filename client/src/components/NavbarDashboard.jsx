c9f248f0e035e8cb7827b50697e4ae3677f4373d
Don Alvin <alvindon41@gmail.com>
2023-02-04 08:55:28 +0300

    Dashboard page

--- client/src/components/NavbarDashboard.jsx
+++ client/src/components/NavbarDashboard.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 
 const NavbarDashboard = () => {
   return (
-    <Flex as='nav' p='10px' alignItems='center' justify='space-between' mb='40px' w='100%' px='30px'>
+    <Flex as='nav' py='30px' alignItems='center' justify='space-between' mb='40px' w='100%' px='40px' borderBottom='2px' borderColor='brand.900'>
         <Heading as='h1' display='flex' alignItems='center' gap={4} color='brand.900'>
           <Image src='/images/icon.svg' alt='Profile Picture' fontSize='12px' w={{base: '30px', lg: '50px'}} h={{base: '30px', lg: '50px'}} objectFit='cover' borderRadius="50%" />
           Proma
         </Heading>
         <HStack spacing='20px'>
-            <Avatar name='Kevin' bg='brand.500' src='/images/profile.png'>
-              <AvatarBadge width='1.3em' bg='teal.500'>
+            <Avatar name='Kevin' bg='brand.500' src='/images/profile.jpg'>
+              <AvatarBadge width='1.3em' bg='teal.900'>
                   <Text fontSize='xs' color='white'>3</Text>
               </AvatarBadge>
             </Avatar>
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,53 @@
+import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Divider, Flex, Heading, HStack, SimpleGrid, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 import React from 'react'
+import tasks from '../../data/data'
+import { ViewIcon, EditIcon } from '@chakra-ui/icons'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Tabs colorScheme='brand.900' variant='enclosed'>
+      <TabList>
+        <Tab _selected={{color: 'white', bg: 'brand.900'}} mr='20px' color='brand.900'>All Tasks</Tab>
+        <Tab _selected={{color: 'white', bg: 'brand.900'}} mr='20px' color='brand.900'>Under progress</Tab>
+        <Tab _selected={{color: 'white', bg: 'brand.900'}} mr='20px' color='brand.900'>Under Review</Tab>
+        <Tab _selected={{color: 'white', bg: 'brand.900'}} mr='20px' color='brand.900'>Completed</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+        <SimpleGrid spacing={8} minChildWidth={300}>
+      {tasks && tasks.map(task => (
+        <Card key={task.id} borderTop='8px' borderColor='brand.900' bg='white'>
+          <CardHeader>
+            <Flex gap={5}>
+              <Avatar name={task.author} bg='brand.500' src={task.img} />
+              <Box>
+                <Heading as='h3' size='sm'>{task.title}</Heading>
+                <Text>by {task.author}</Text>
+              </Box>
+            </Flex>
+          </CardHeader>
+
+          <CardBody color='gray.500'>
+            <Text>{task.description}</Text>
+          </CardBody>
+            <Divider borderColor='gray.200'/>
+          <CardFooter>
+            <HStack>
+              <Button variant="ghost" leftIcon={<ViewIcon />}>Watch</Button>
+              <Button variant="ghost" leftIcon={<EditIcon />}>Comment</Button>
+            </HStack>
+          </CardFooter>
+        </Card>
+      ))}
+    </SimpleGrid>
+        </TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </TabPanels>
+    </Tabs>
   )
 }
+
 
 export default Dashboard
