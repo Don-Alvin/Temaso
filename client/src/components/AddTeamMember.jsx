e2b16285b127db902dbdfeeeeaba4ce58e9b827f
Don Alvin <alvindon41@gmail.com>
2023-02-08 15:58:10 +0300

    Add team member

--- client/src/components/AddTeamMember.jsx
+++ client/src/components/AddTeamMember.jsx
@@ -1,9 +1,29 @@
 import { Box, Button, FormControl, FormLabel, Input, Icon, Text, Flex } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Link } from 'react-router-dom'
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 
 const AddTeamMember = () => {
+  const [name, setName] = useState('')
+  const [position, setPosition] = useState('')
+  const [positionIsValid, setPositionIsValid] = useState(true)
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    if (position.toLowerCase() !== 'product designer' && position.toLowerCase() !== 'backend developer' && position.toLowerCase() !== 'frontend developer'){
+        setPositionIsValid(false)
+        setPosition('')
+        return
+    }else {
+      setPositionIsValid(true)
+    }
+
+    setName('')
+    setPosition('')
+    setPositionIsValid(true)
+  }
+
   return (
     <Box px={{lg: '100px'}} py={{lg: '30px'}}>
         <Link to='/dashboard/team'>
@@ -12,16 +32,42 @@ const AddTeamMember = () => {
             <Text color='gray.500'>Back to team</Text>
           </Flex>
         </Link>
-        <Form>
+        <Form method='post' onSubmit={submitHandler}>
             <FormControl w={{lg:'70%'}} mb='30px' isRequired>
                 <FormLabel color='gray.500'>Name</FormLabel>
-                <Input type='text' name='name' placeholder='Enter name' outlineColor='brand.900' border='none'/>
+                <Input 
+                  type='text' 
+                  name='name' 
+                  placeholder='Enter name' 
+                  outlineColor='brand.900' 
+                  border='none'
+                  onChange={event => setName(event.target.value)}
+                  value={name}
+                />
             </FormControl>
             <FormControl w={{lg:'70%'}} mb='30px' isRequired>
                 <FormLabel color='gray.500'>Position</FormLabel>
-                <Input type='text' name='position' placeholder='Enter position' outlineColor='brand.900' border='none'/>
+                <Input 
+                  type='text' 
+                  name='position' 
+                  placeholder='Enter position' 
+                  outlineColor='brand.900' 
+                  _active={{outline: 'none'}}
+                  border='none'
+                  onChange={event => setPosition(event.target.value)}
+                  value={position}
+                />
+                {!positionIsValid && <Text as='small' color='red'>* VALID POSITIONS ARE PRODUCT DESIGNER, MARKETER, BACKEND DEVELOPER AND FRONTEND DEVELOPER</Text>}
             </FormControl>
-            <Button type='submit' bg='brand.900' color='white'>Add New</Button>
+            <Button 
+              type='submit' 
+              bg='brand.900' 
+              color='white' 
+              _hover={{bg: 'brand.700'}}
+
+            >
+              Add New
+            </Button>
         </Form>
     </Box>
   )
