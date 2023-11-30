012fcd3a1210c77099f276de34afd2f8af68207f
Don Alvin <alvindon41@gmail.com>
2023-11-30 22:02:15 +0300

    refactor: auth UI

--- src/Features/Auth/Register.jsx
+++ src/Features/Auth/Register.jsx
@@ -18,10 +18,6 @@ const Register = () => {
   const navigate = useNavigate()
   const provider = new GoogleAuthProvider()
 
-  const handlePassword = () => {
-    setPasswordVisible(!passwordVisible)
-  }
-
   const onSubmit = async() => {
     try {
       const res = await createUserWithEmailAndPassword(auth, values.email, values.password);
@@ -76,50 +72,55 @@ const Register = () => {
       <MetaData title={'Register an account'} />
       <div className="flex justify-center flex-col items-center border p-6 h-[auto] my-20 gap-2  border-teal-700 rounded-lg shadow-teal-700 shadow-md bg-white opacity-90 w-[90%] md:w-auto">
         <div className="title flex flex-col items-center">
-          <h4 className="text-3xl text-gray-700 text-center">Explore more by joining us</h4>
+          <h4 className="text-xl text-gray-700 text-center">Explore more by joining us</h4>
         </div>
         <button 
-          className="shadow flex items-center rounded"
+          className="shadow flex items-center rounded bg-[#00396B]"
           onClick={registerWithGoogle}
         >
-          <FcGoogle className="w-6"/>
-          <p className="text-white bg-[#00396B] p-1 font-semibold rounded-r">Register with google</p>
+          <div className="p-2">
+            <FcGoogle className="w-8 h-8"/>
+          </div>
+          
+          <p className="text-white p-2 font-semibold rounded-r">Sign in with google</p>
         </button>
         <span className="text-gray-700 font-semibold">OR</span>
         <form className='w-[90%]' onSubmit={handleSubmit}>
-              <div className='flex flex-col gap-4 items-center'>
-                <div className='flex flex-col gap-2 w-[100%]'>
-                  <label className="text-gray-700 text-md">Name</label>
-                  <input
-                    className={`p-2 border rounded-lg ${errors.displayName && touched.displayName ? `outline outline-1 outline-red-700` : `outline-teal-700`}`}
-                    type="text"
-                    value={values.displayName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    id="displayName"
-                    placeholder='Enter your username'
-                    autoComplete="off"
-                  />
-                  {errors.displayName && touched.displayName && <p className='text-red-700'>{errors.displayName}</p>}
+              <div className='flex flex-col gap-4 p-4'>
+                <div className="flex-col flex md:flex-row gap-4">
+                  <div className='flex flex-col gap-2 w-[100%]'>
+                    <label className="text-gray-700 text-md">Name</label>
+                    <input
+                      className={`p-2 border rounded-lg bg-white placeholder:text-gray-700 ${errors.displayName && touched.displayName ? `outline outline-1 outline-red-700` : `outline-teal-700`}`}
+                      type="text"
+                      value={values.displayName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      id="displayName"
+                      placeholder='Enter your username'
+                      autoComplete="off"
+                    />
+                    {errors.displayName && touched.displayName && <p className='text-red-700'>{errors.displayName}</p>}
+                  </div>
+                  <div className='flex flex-col gap-2 w-[100%]'>
+                    <label className="text-gray-700 text-md">Email</label>
+                    <input
+                      className={`p-2 border rounded-lg bg-white placeholder:text-gray-700 ${errors.email && touched.email ? `outline outline-1 outline-red-700` : `outline-teal-700`}`}
+                      type="email"
+                      id="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      placeholder='Enter your email'
+                      autoComplete="off"
+                    />
+                    {errors.email && touched.email && <p className='text-red-700'>{errors.email}</p>}
+                  </div>
                 </div>
-                <div className='flex flex-col gap-2 w-[100%]'>
-                  <label className="text-gray-700 text-md">Email</label>
-                  <input
-                    className={`p-2 border rounded-lg ${errors.email && touched.email ? `outline outline-1 outline-red-700` : `outline-teal-700`}`}
-                    type="email"
-                    id="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    placeholder='Enter your email'
-                    autoComplete="off"
-                  />
-                  {errors.email && touched.email && <p className='text-red-700'>{errors.email}</p>}
-                </div>
-                  <div className='flex flex-col gap-2 relative w-[100%]'>
+                  <div className='flex flex-col gap-2 relative w-full'>
                     <label className="text-gray-700 text-md">Password</label>
                     <input
-                      className={`p-2 border rounded-lg ${errors.password && touched.password ? `outline outline-1 outline-red-700` : `outline-teal-700`}`}
+                      className={`p-2 border rounded-lg bg-white placeholder:text-gray-700 ${errors.password && touched.password ? `outline outline-1 outline-red-700` : `outline-teal-700`}`}
                       type={!passwordVisible ? 'password' : 'text'}
                       id="password"
                       onChange={handleChange}
@@ -128,8 +129,6 @@ const Register = () => {
                       placeholder='Enter your password'
                       autoComplete="off"
                     />
-                    {passwordVisible && <AiOutlineEyeInvisible  className={`absolute ${errors.password && touched.password && `top-[42%]`} top-[62%] right-3`} onClick={handlePassword}/>}
-                    {!passwordVisible && <AiOutlineEye  className={`absolute ${errors.password && touched.password && `top-[42%]`} top-[62%] right-3`} onClick={handlePassword}/>}
                     {errors.password && touched.password && <p className='text-red-700'>{errors.password}</p>}
                   </div>
               <button type="submit" disabled={isSubmitting} className='bg-[#00396B] rounded-lg p-3 text-white font-semibold w-[100%]'>Register</button>
